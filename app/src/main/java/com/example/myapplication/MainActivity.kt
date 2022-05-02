5be91e33e6024aece7b4e5c8d229621e61dceb97
Jeong MinGu <mingu830@gmail.com>
2022-05-03 00:01:54 +0900

    Error 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import android.widget.Button
 import android.widget.TextView
@@ -10,16 +11,18 @@ import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     private val changeActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){  }
-
+ //   private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+   //     Log.e(TAG,"Log출력")
 
         val toastButton: Button = findViewById(R.id.toast)
         val countButton: Button = findViewById(R.id.count)
         val randomButton: Button = findViewById(R.id.random)
         val countNumEditable: TextView=findViewById(R.id.number)    //xml파일의 number를 Editable형으로 받음
-        var countNum: Int = countNumEditable.toString().toInt()     //Editable형을 String으로 변환 후 Int로 변환
+        var countNum:Int = 0
 
         toastButton.setOnClickListener{
             Toast.makeText(this, "토스트 메시지 입니다!", Toast.LENGTH_LONG).show()
@@ -32,6 +35,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondaryActivity:: class.java)
             intent.putExtra("countNum",countNum)
             changeActivity.launch(intent)
+            countNum=intent.getIntExtra("randNum",0)
+            countNumEditable.text=countNum.toString()
         }
+
     }
 }
