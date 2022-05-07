aa83ee33cc4d680ebb166262e3f815d601becc8f
Jeong MinGu <mingu830@gmail.com>
2022-05-07 15:32:18 +0900

    두번째 Activity에서 값 가져오기 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,41 +3,48 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
-    private val changeActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){  }
- //   private val TAG = "MainActivity"
+    var countNum:Int = 0
+    private lateinit var countTextView: TextView
+    private lateinit var changeActivity:ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-   //     Log.e(TAG,"Log출력")
-
         val toastButton: Button = findViewById(R.id.toast)
         val countButton: Button = findViewById(R.id.count)
         val randomButton: Button = findViewById(R.id.random)
-        val countNumEditable: TextView=findViewById(R.id.number)    //xml파일의 number를 Editable형으로 받음
-        var countNum:Int = 0
+
+        countTextView=findViewById(R.id.number)
+        countTextView.text=countNum.toString()
 
         toastButton.setOnClickListener{
             Toast.makeText(this, "토스트 메시지 입니다!", Toast.LENGTH_LONG).show()
         }
+
         countButton.setOnClickListener{
             countNum++
-            countNumEditable.text=countNum.toString()
-        }
-        randomButton.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity:: class.java)
-            intent.putExtra("countNum",countNum)
-            changeActivity.launch(intent)
-            countNum=intent.getIntExtra("randNum",0)
-            countNumEditable.text=countNum.toString()
+            countTextView.text=countNum.toString()
         }
 
+        changeActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode== RESULT_OK){
+                val getNum =it.data!!.getIntExtra("randNum",0)
+                countNum=getNum.toString().toInt()
+                countTextView.text=countNum.toString()
+            }
+        }
+
+        randomButton.setOnClickListener {
+            val intent = Intent(this, SecondaryActivity:: class.java)
+            intent.putExtra("countNum", countNum)
+            changeActivity.launch(intent)
+        }
     }
 }
